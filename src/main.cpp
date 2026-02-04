7047fa5a881f5dafce2eadb0cbad0d1723c82839
Stutimittal_17 <stuti.mittal_cs.aimml24@gla.ac.in>
2026-02-04 09:57:22 +0530

    Add pin to config main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
 #include <Arduino.h>
+#define ldrPin A0
 void setup() {
-// write your initialization code here
+
+    Serial.begin(9600);
+    pinMode(ldrPin, INPUT);
+    Serial.println("LDR PIN");
 }
 
 void loop() {
+    int ldrRaw = analogRead(ldrPin);
+    int lightPercent = map(ldrRaw, 0, 100, 0, 100);
+    Serial.println("LDR RAW: ");
+    Serial.println(ldrRaw);
+    Serial.println("Light PERCENT: ");
+    Serial.println(lightPercent);
+    Serial.println("%");
+    delay(1000);
 // write your code here
 }
